208d936b617738a1c56e6d984f47bbf72af62a11
Jorge Javier Araya Navarro <jorge@esavara.cr>
2025-05-23 20:21:03 -0600

    git: Install LFS along with pre-commit hook
    - https://github.com/cachix/git-hooks.nix/issues/476

--- flake.nix
+++ flake.nix
@@ -55,9 +55,22 @@
 
               hugo
               djlint
+
+              git
+              git-lfs
+
+              python3
+
+              (pkgs.writeScriptBin "project-git-lfs-hook-installer" (builtins.readFile ./etc/scripts/lfs-hook.py))
             ];
 
-            inherit (self.checks.${pkgs.system}.pre-commit-check) shellHook;
+            shellHook =
+              self.checks.${pkgs.system}.pre-commit-check.shellHook
+              + ''
+                echo "Injecting Git LFS hooks..."
+                for hook in pre-push post-checkout post-commit post-merge; do
+                  project-git-lfs-hook-installer --stage $hook
+                done'';
             buildInputs = self.checks.${pkgs.system}.pre-commit-check.enabledPackages;
           };
         }
@@ -83,8 +96,6 @@
                 entry = "${pkgs.djlint}/bin/djlint --profile=golang";
                 files = "\\.html$";
               };
-              # check for broken links
-              mkdocs-linkcheck.enable = true;
               # check markdown format is correct
               markdownlint.enable = true;
               # other checkers
